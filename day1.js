6d9ece5b27728d35fc3c20f6311880424521e109
vandana2238 <vandana.c38114@gmail.com>
2024-07-19 13:23:45 +0530

    Day1.js file added

---
+++ day1.js
@@ -0,0 +1,37 @@
+//declaration of variable using var
+var x = 14;
+console.log(x);
+
+//declaration of variable using let
+let str = "hello world"
+console.log(str);
+
+//declaration using const
+const y = false;
+console.log(y);
+
+//ACTIVITY-3
+
+let num = 13;
+let word = "apple";
+let bool = true;
+let obj = [1,2,3,4,5];
+console.log(typeof num);
+console.log(typeof word);
+console.log(typeof bool);
+console.log(typeof obj);
+
+//ACTIVITY-4
+let a;
+a = 14;
+console.log(a);
+a = 24;
+console.log(a);
+
+//ACTIVITY-5
+const b = 10;
+b = 20;
+console.log(b); //type error occured
+
+
+
